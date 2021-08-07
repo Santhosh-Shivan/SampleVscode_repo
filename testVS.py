24fe39b91bfb0044fa88c87e82f096b28185f0f1
Santhosh-Shivan <santhoshshivang@gmail.com>
2021-08-07 13:59:41 +0200

    Removed some code

--- testVS.py
+++ testVS.py
@@ -1,18 +1,5 @@
-import numpy as np
-import tensorflow as tf
 import os
 import sys
 
-"""dir_path = os.path.dirname(os.path.realpath(__file__))
-for f in os.listdir(dir_path):
-    print(f)"""
 
 print(os.getcwd())
-"""print(dir_path)
-print(sys.executable)
-print("sandy")
-
-b = 25
-a = input("ENter ur name")
-
-print(a)"""
